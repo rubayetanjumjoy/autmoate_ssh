618dae48d6fe6383fab524ce88600434200b4a6c
Quazi Rubayet Anjum Joy <quazi.joy@viewqwest.com>
2024-01-10 16:36:20 +0800

    s

--- automate.py
+++ automate.py
@@ -1,44 +1,10 @@
-import paramiko
+import subprocess
 
-# Server details
-ip_address = "10.21.4.160"
-username = "leon.paingheinh"
-password = "oXWwe8pZ"
+# Run the 'ls' command
+result = subprocess.run(['ls'], stdout=subprocess.PIPE, text=True)
 
-# Command to be executed
-command = "ls"
+# Save the output to a text file
+with open('ls_output.txt', 'w') as file:
+    file.write(result.stdout)
 
-try:
-    # Create an SSH client
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    # Connect to the server
-    ssh_client.connect(ip_address, username=username, password=password)
-
-    # Execute the command
-    stdin, stdout, stderr = ssh_client.exec_command(command)
-
-    # Read the output
-    result = stdout.read().decode('utf-8')
-
-    # Print command output
-    print(result)
-
-    # Save result to a file
-    with open("ls_output.txt", "w") as file:
-        file.write(result)
-
-    print("Command executed successfully.")
-
-except paramiko.AuthenticationException:
-    print("Authentication failed.")
-except paramiko.SSHException as e:
-    print(f"Unable to establish SSH connection: {e}")
-except Exception as e:
-    print(f"Error executing command: {e}")
-
-finally:
-    # Close the SSH connection
-    if ssh_client:
-        ssh_client.close()
+print("ls command output has been saved to ls_output.txt")
