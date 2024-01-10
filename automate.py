eb2fc4280fb57273d209eec5ea785611f42dde6f
Quazi Rubayet Anjum Joy <quazi.joy@viewqwest.com>
2024-01-10 16:24:33 +0800

    c'

--- automate.py
+++ automate.py
@@ -1,20 +1,49 @@
 import subprocess
 
-# Specify the file name where you want to save the ls command output
-output_file = 'ls_output.txt'
+# Server details
+ip_address = "10.21.4.160"
+username = "leon.paingheinh"
+password = "oXWwe8pZ"
 
-# Using subprocess to run the ls command and capture the output
+# Command to be executed
+command = "ls"
+
 try:
-    # Run the ls command with subprocess and capture the output
-    ls_output = subprocess.check_output(['ls'], text=True)
+    # Spawn an SSH process
+    ssh_process = subprocess.Popen(
+        f'ssh {username}@{ip_address} "{command}"', 
+        shell=True, 
+        stdin=subprocess.PIPE, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE,
+        universal_newlines=True
+    )
 
-    # Write the output to the specified file
-    with open(output_file, 'w') as file:
-        file.write(ls_output)
+    # Provide the password
+    ssh_process.stdin.write(password + '\n')
+    ssh_process.stdin.flush()
+    print("Successfully SSHed into the server")
 
-    print(f"ls command output successfully written to {output_file}")
+    # Wait for the process to complete and get output/error
+    stdout, stderr = ssh_process.communicate()
+
+    # Print command output
+    print("Standard Output:")
+    print(stdout)
+
+    # Print command error (if any)
+    print("Standard Error:")
+    print(stderr)
+
+    # Save result to a file
+    with open("ls_output.txt", "w") as file:
+        file.write(stdout)
+
+    print("Command executed successfully.")
 
 except subprocess.CalledProcessError as e:
-    print(f"Error while running ls command: {e}")
-except IOError as e:
-    print(f"Error writing to file {output_file}: {e}")
+    print(f"Error executing command: {e}")
+
+finally:
+    # No need to explicitly close the SSH process here
+    pass
